47e46fb92226b53961f2d8fe07902ff7cecffd6e
Jhonny Moreira <xDeniedAccess@gmail.com>
2015-10-30 15:57:03 -0200

    Aww yiis :fire:

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
-var nav__scrollspy = (function changeBackgroundColor (el, target, cl) {
+var nav__padding = function addPadding (nav, target) {
+  nav = document.querySelector(nav);
+  target = document.querySelector(target);
+
+  var navDimensions = {
+    height: nav.scrollHeight
+  };
+
+  var windowDimensions = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  };
+
+  window.onresize = function updateValues () {
+    navDimensions['height'] = nav.scrollHeight;
+
+    windowDimensions['width'] = window.innerWidth;
+    windowDimensions['height'] = window.innerHeight;
+
+    if (windowDimensions['width'] <= 768 || windowDimensions['height'] <= 550) {
+      target.style.paddingTop = (navDimensions['height'] + 30) + 'px';
+    } else {
+      target.style.paddingTop = '';
+    }
+  };
+
+  /* Executes the first time and left everything to onresize function */
+  if (windowDimensions['width'] <= 768 || windowDimensions['height'] <= 550) {
+    target.style.paddingTop = (navDimensions['height'] + 30) + 'px';
+  } else {
+    target.style.paddingTop = '';
+  }
+}('.nav', '.transparency');
+
+var nav__scrollspy = function changeBackgroundColor (el, target, cl) {
   /* Select the position of the 'target' element */
   var targetPosition = document.querySelector(target).offsetTop;
 
@@ -21,4 +55,4 @@ var nav__scrollspy = (function changeBackgroundColor (el, target, cl) {
     if ( scrollPosition >= targetPosition ) el.classList.add(cl);
     else                                    el.classList.remove(cl);
   };
-})('.nav', '.feature', 'nav--light');
+}('.nav', '.features', 'nav--light');
